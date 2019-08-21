bef03ef3faf9a1b4fc6ca53b4aed63675fe7ba5b
Peter Hunt <pehunt@redhat.com>
2019-08-21 12:17:44 -0400

    drop dependency which isn't needed
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- test/e2e/storage/testsuites/volumemode.go
+++ test/e2e/storage/testsuites/volumemode.go
@@ -31,7 +31,6 @@ import (
 	"k8s.io/kubernetes/test/e2e/common"
 	"k8s.io/kubernetes/test/e2e/framework"
 	e2elog "k8s.io/kubernetes/test/e2e/framework/log"
-	e2epod "k8s.io/kubernetes/test/e2e/framework/pod"
 	"k8s.io/kubernetes/test/e2e/storage/testpatterns"
 )
 
